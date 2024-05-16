e30afa07476a9fb6db2a7ae15b9da99309cff671
Enrico Corticelli <enrico.corticelli@gmail.com>
2024-05-16 10:19:16 +0200

    Progetto iniziale next.js

---
+++ progetti-dadaumpa/src/app/components/Footer.js
@@ -0,0 +1,77 @@
+"use client";
+
+import React, { useState } from "react";
+
+const Footer = () => {
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const images = [
+    "https://source.unsplash.com/collection/1346951/800x800?sig=1",
+    "https://source.unsplash.com/collection/1346951/800x800?sig=2",
+    "https://source.unsplash.com/collection/1346951/800x800?sig=3",
+    "https://source.unsplash.com/collection/1346951/800x800?sig=4",
+    "https://source.unsplash.com/collection/1346951/800x800?sig=5",
+    "https://source.unsplash.com/collection/1346951/800x800?sig=6",
+    "https://source.unsplash.com/collection/1346951/800x800?sig=7",
+    "https://source.unsplash.com/collection/1346951/800x800?sig=8",
+    "https://source.unsplash.com/collection/1346951/800x800?sig=9",
+  ];
+
+  const increment = () => {
+    setCurrentIndex((prevIndex) => {
+      if (prevIndex === images.length - 6) {
+        return 0;
+      } else {
+        return prevIndex + 1;
+      }
+    });
+  };
+
+  const decrement = () => {
+    setCurrentIndex((prevIndex) => {
+      if (prevIndex === 0) {
+        return images.length - 6;
+      } else {
+        return prevIndex - 1;
+      }
+    });
+  };
+
+  return (
+    <footer className="w-full border-t bg-white pb-12">
+      <div className="relative w-full flex items-center invisible md:visible md:pb-12">
+        {images.slice(currentIndex, currentIndex + 6).map((image, index) => (
+          <img key={index} className="w-1/6 hover:opacity-75" src={image} />
+        ))}
+        <button
+          className="absolute bg-blue-800 hover:bg-blue-700 text-white text-2xl font-bold hover:shadow rounded-full w-16 h-16 ml-12"
+          onClick={decrement}>
+          &#8592;
+        </button>
+        <button
+          className="absolute right-0 bg-blue-800 hover:bg-blue-700 text-white text-2xl font-bold hover:shadow rounded-full w-16 h-16 mr-12"
+          onClick={increment}>
+          &#8594;
+        </button>
+      </div>
+      <div className="w-full container mx-auto flex flex-col items-center">
+        <div className="flex flex-col md:flex-row text-center md:text-left md:justify-between py-6">
+          <a href="#" className="uppercase px-3">
+            About Us
+          </a>
+          <a href="#" className="uppercase px-3">
+            Privacy Policy
+          </a>
+          <a href="#" className="uppercase px-3">
+            Terms & Conditions
+          </a>
+          <a href="#" className="uppercase px-3">
+            Contact Us
+          </a>
+        </div>
+        <div className="uppercase pb-6">&copy; myblog.com</div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
---
+++ progetti-dadaumpa/src/app/components/Header.js
@@ -0,0 +1,104 @@
+"use client";
+
+import React, { useState } from "react";
+
+const Header = () => {
+  const [open, setOpen] = useState(false);
+
+  return (
+    <>
+      {/* Navigation */}
+      <nav className="w-full py-4 bg-blue-800 shadow">
+        <div className="w-full container mx-auto flex flex-wrap items-center justify-between">
+          <nav>
+            <ul className="flex items-center justify-between font-bold text-sm text-white uppercase no-underline">
+              <li>
+                <a
+                  className="hover:text-gray-200 hover:underline px-4"
+                  href="#">
+                  Shop
+                </a>
+              </li>
+              <li>
+                <a
+                  className="hover:text-gray-200 hover:underline px-4"
+                  href="#">
+                  About
+                </a>
+              </li>
+            </ul>
+          </nav>
+          <div className="flex items-center text-lg no-underline text-white pr-6">
+            <a className="" href="#">
+              <i className="fab fa-facebook"></i>
+            </a>
+            <a className="pl-6" href="#">
+              <i className="fab fa-instagram"></i>
+            </a>
+            <a className="pl-6" href="#">
+              <i className="fab fa-twitter"></i>
+            </a>
+            <a className="pl-6" href="#">
+              <i className="fab fa-linkedin"></i>
+            </a>
+          </div>
+        </div>
+      </nav>
+
+      {/* Text Header */}
+      <header className="w-full container mx-auto">
+        <div className="flex flex-col items-center py-12">
+          <a
+            className="font-bold text-gray-800 uppercase hover:text-gray-700 text-5xl"
+            href="#">
+            Minimal Blog
+          </a>
+          <p className="text-lg text-gray-600">Lorem Ipsum Dolor Sit Amet</p>
+        </div>
+      </header>
+
+      {/* Topic Nav */}
+      <nav className="w-full py-4 border-t border-b bg-gray-100">
+        <div className="block sm:hidden">
+          <a
+            href="#"
+            className="block md:hidden text-base font-bold uppercase text-center flex justify-center items-center"
+            onClick={() => setOpen(!open)}>
+            Topics{" "}
+            <i
+              className={`fas ml-2 ${
+                open ? "fa-chevron-down" : "fa-chevron-up"
+              }`}></i>
+          </a>
+        </div>
+        <div
+          className={`${
+            open ? "block" : "hidden"
+          } w-full flex-grow sm:flex sm:items-center sm:w-auto`}>
+          <div className="w-full container mx-auto flex flex-col sm:flex-row items-center justify-center text-sm font-bold uppercase mt-0 px-6 py-2">
+            <a href="#" className="hover:bg-gray-400 rounded py-2 px-4 mx-2">
+              Technology
+            </a>
+            <a href="#" className="hover:bg-gray-400 rounded py-2 px-4 mx-2">
+              Automotive
+            </a>
+            <a href="#" className="hover:bg-gray-400 rounded py-2 px-4 mx-2">
+              Finance
+            </a>
+            <a href="#" className="hover:bg-gray-400 rounded py-2 px-4 mx-2">
+              Politics
+            </a>
+            <a href="#" className="hover:bg-gray-400 rounded py-2 px-4 mx-2">
+              Culture
+            </a>
+            <a href="#" className="hover:bg-gray-400 rounded py-2 px-4 mx-2">
+              Sports
+            </a>
+          </div>
+        </div>
+      </nav>
+    </>
+  );
+};
+
+export default Header;
