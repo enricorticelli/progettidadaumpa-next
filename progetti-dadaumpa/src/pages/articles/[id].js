231d20312219a8925222b996158832d04f2051c7
Enrico Corticelli <enrico.corticelli@gmail.com>
2024-05-17 15:53:14 +0200

    Fixa css e classNames

--- progetti-dadaumpa/src/pages/articles/[id].js
+++ progetti-dadaumpa/src/pages/articles/[id].js
@@ -11,27 +11,31 @@ export default function Article() {
 
   return (
     <Layout>
-      <section class="w-full md:w-2/3 flex flex-col items-center px-3">
-        <article class="flex flex-col shadow my-4">
-          {/* <a href="#" class="hover:opacity-75">
+      <section classNames="w-full md:w-2/3 flex flex-col items-center px-3">
+        <article classNames="flex flex-col shadow my-4">
+          {/* <a href="#" classNames="hover:opacity-75">
                     <img src="https://source.unsplash.com/collection/1346951/1000x500?sig=1">
                 </a> */}
-          <div class="bg-white flex flex-col justify-start p-6">
-            <a href="#" class="text-blue-700 text-sm font-bold uppercase pb-4">
+          <div classNames="bg-white flex flex-col justify-start p-6">
+            <a
+              href="#"
+              classNames="text-blue-700 text-sm font-bold uppercase pb-4">
               Technology
             </a>
-            <a href="#" class="text-3xl font-bold hover:text-gray-700 pb-4">
+            <a
+              href="#"
+              classNames="text-3xl font-bold hover:text-gray-700 pb-4">
               Lorem Ipsum Dolor Sit Amet Dolor Sit Amet
             </a>
-            <p href="#" class="text-sm pb-8">
+            <p href="#" classNames="text-sm pb-8">
               By{" "}
-              <a href="#" class="font-semibold hover:text-gray-800">
+              <a href="#" classNames="font-semibold hover:text-gray-800">
                 David Grzyb
               </a>
               , Published on April 25th, 2020
             </p>
-            <h1 class="text-2xl font-bold pb-3">Introduction</h1>
-            <p class="pb-3">
+            <h1 classNames="text-2xl font-bold pb-3">Introduction</h1>
+            <p classNames="pb-3">
               Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur
               vel neque non libero suscipit suscipit eu eu urna. Proin bibendum
               urna mattis ante malesuada ultrices. Etiam in turpis vitae elit
@@ -45,8 +49,8 @@ export default function Article() {
               venenatis mollis et at massa. Pellentesque sem lacus, malesuada
               vel hendrerit molestie, mollis vel elit.
             </p>
-            <h1 class="text-2xl font-bold pb-3">Heading</h1>
-            <p class="pb-3">
+            <h1 classNames="text-2xl font-bold pb-3">Heading</h1>
+            <p classNames="pb-3">
               Vivamus nec facilisis elit, quis congue justo. In non augue ex.
               Aenean pretium facilisis hendrerit. Sed sed imperdiet dui. Etiam
               faucibus a diam sed vehicula. Nullam commodo lacus tincidunt,
@@ -59,7 +63,7 @@ export default function Article() {
               Pellentesque tristique orci nunc, vitae fermentum nibh luctus eu.
               Mauris condimentum justo sed ipsum egestas varius.
             </p>
-            <p class="pb-3">
+            <p classNames="pb-3">
               Sed sagittis odio a volutpat feugiat. Cras aliquam varius justo, a
               rhoncus ante bibendum id. Nulla maximus nisl sed enim maximus, ut
               dictum lectus hendrerit. Fusce venenatis tincidunt eros. Phasellus
@@ -72,7 +76,7 @@ export default function Article() {
               ante. Duis molestie mollis eros, eu sollicitudin mauris lobortis
               quis.
             </p>
-            <p class="pb-3">
+            <p classNames="pb-3">
               Vivamus sed neque nec massa scelerisque imperdiet eget id sapien.
               Fusce elementum mi id malesuada luctus. Proin quis lorem id leo
               porta interdum non ac nisl. Integer nulla sem, ultrices sed neque
@@ -84,8 +88,8 @@ export default function Article() {
               consequat vel. Cras a mi eu ex luctus malesuada quis eu ante.
               Aliquam erat volutpat.
             </p>
-            <h1 class="text-2xl font-bold pb-3">Conclusion</h1>
-            <p class="pb-3">
+            <h1 classNames="text-2xl font-bold pb-3">Conclusion</h1>
+            <p classNames="pb-3">
               Donec vulputate auctor leo lobortis congue. Sed elementum pharetra
               turpis. Nulla at condimentum odio. Vestibulum ullamcorper enim
               eget porttitor bibendum. Proin eros nibh, maximus vitae nisi a,
@@ -98,47 +102,47 @@ export default function Article() {
           </div>
         </article>
 
-        <div class="w-full flex pt-6">
+        <div classNames="w-full flex pt-6">
           <a
             href="#"
-            class="w-1/2 bg-white shadow hover:shadow-md text-left p-6">
-            <p class="text-lg text-blue-800 font-bold flex items-center">
-              <i class="fas fa-arrow-left pr-1"></i> Previous
+            classNames="w-1/2 bg-white shadow hover:shadow-md text-left p-6">
+            <p classNames="text-lg text-blue-800 font-bold flex items-center">
+              <i classNames="fas fa-arrow-left pr-1"></i> Previous
             </p>
-            <p class="pt-2">Lorem Ipsum Dolor Sit Amet Dolor Sit Amet</p>
+            <p classNames="pt-2">Lorem Ipsum Dolor Sit Amet Dolor Sit Amet</p>
           </a>
           <a
             href="#"
-            class="w-1/2 bg-white shadow hover:shadow-md text-right p-6">
-            <p class="text-lg text-blue-800 font-bold flex items-center justify-end">
-              Next <i class="fas fa-arrow-right pl-1"></i>
+            classNames="w-1/2 bg-white shadow hover:shadow-md text-right p-6">
+            <p classNames="text-lg text-blue-800 font-bold flex items-center justify-end">
+              Next <i classNames="fas fa-arrow-right pl-1"></i>
             </p>
-            <p class="pt-2">Lorem Ipsum Dolor Sit Amet Dolor Sit Amet</p>
+            <p classNames="pt-2">Lorem Ipsum Dolor Sit Amet Dolor Sit Amet</p>
           </a>
         </div>
 
-        <div class="w-full flex flex-col text-center md:text-left md:flex-row shadow bg-white mt-10 mb-10 p-6">
-          {/* <div class="w-full md:w-1/5 flex justify-center md:justify-start pb-4">
-                    <img src="https://source.unsplash.com/collection/1346951/150x150?sig=1" class="rounded-full shadow h-32 w-32">
+        <div classNames="w-full flex flex-col text-center md:text-left md:flex-row shadow bg-white mt-10 mb-10 p-6">
+          {/* <div classNames="w-full md:w-1/5 flex justify-center md:justify-start pb-4">
+                    <img src="https://source.unsplash.com/collection/1346951/150x150?sig=1" classNames="rounded-full shadow h-32 w-32">
                 </div> */}
-          <div class="flex-1 flex flex-col justify-center md:justify-start">
-            <p class="font-semibold text-2xl">David</p>
-            <p class="pt-2">
+          <div classNames="flex-1 flex flex-col justify-center md:justify-start">
+            <p classNames="font-semibold text-2xl">David</p>
+            <p classNames="pt-2">
               Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur
               vel neque non libero suscipit suscipit eu eu urna.
             </p>
-            <div class="flex items-center justify-center md:justify-start text-2xl no-underline text-blue-800 pt-4">
-              <a class="" href="#">
-                <i class="fab fa-facebook"></i>
+            <div classNames="flex items-center justify-center md:justify-start text-2xl no-underline text-blue-800 pt-4">
+              <a classNames="" href="#">
+                <i classNames="fab fa-facebook"></i>
               </a>
-              <a class="pl-4" href="#">
-                <i class="fab fa-instagram"></i>
+              <a classNames="pl-4" href="#">
+                <i classNames="fab fa-instagram"></i>
               </a>
-              <a class="pl-4" href="#">
-                <i class="fab fa-twitter"></i>
+              <a classNames="pl-4" href="#">
+                <i classNames="fab fa-twitter"></i>
               </a>
-              <a class="pl-4" href="#">
-                <i class="fab fa-linkedin"></i>
+              <a classNames="pl-4" href="#">
+                <i classNames="fab fa-linkedin"></i>
               </a>
             </div>
           </div>
